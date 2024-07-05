10d3c262d14f57fdf560b546b856ff6cae333d8f
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-04 17:21:48 -0700

    add flatpaks

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -1,3 +1,21 @@
 sudo apt install -y flatpak
 sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install flathub com.belmoussaoui.Authenticator
+flatpak install flathub com.usebottles.bottles
+flatpak install flathub app.drey.Warp
+flatpak install flathub hu.irl.cameractrls
+flatpak install flathub io.github.flattool.Warehouse
+flatpak install flathub io.github.giantpinkrobots.varia
+flatpak install flathub io.gitlab.adhami3310.Impression
+flatpak install flathub com.github.KRTirtho.Spotube
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub com.mattjakeman.ExtensionManager
+flatpak install flathub io.missioncenter.MissionCenter
+flatpak install flathub com.naps2.Naps2
+flatpak install flathub io.github.peazip.PeaZip
+flatpak install flathub org.pitivi.Pitivi
+flatpak install flathub org.mozilla.Thunderbird
+flatpak install flathub org.tigervnc.vncviewer
+flatpak install flathub net.werwolv.ImHex
