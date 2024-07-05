672978c6c6a0c2c7bc8757fc1dd8425a9b4ebec8
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 15:08:24 -0700

    update Linuxbrew installation script to include shell environment setup

--- install/app-linuxbrew.sh
+++ install/app-linuxbrew.sh
@@ -3,4 +3,6 @@ cd /tmp/linuxbrew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh -o install.sh
 chmod +x install.sh
 ./install.sh
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> $HOME/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 cd -
