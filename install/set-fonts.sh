13f2921efc3eefd7578266da6a3ceb87e5f855c9
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 15:24:14 -0700

    update GNOME font installation script to use Fira Code

--- install/set-fonts.sh
+++ install/set-fonts.sh
@@ -1,6 +1,6 @@
-sudo apt install fonts-firacode
+sudo apt install fonts-firacode fonts-roboto
 
-gsettings set org.gnome.desktop.wm.preferences titlebar-font "Fira Sans SemiBold 10"
-gsettings set org.gnome.desktop.interface monospace-font-name "Fira Mono Regular 11"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Fira Code SemiBold 10"
+gsettings set org.gnome.desktop.interface monospace-font-name "Fira Code 11"
 gsettings set org.gnome.desktop.interface document-font-name "Roboto Slab Regular 11"
-gsettings set org.gnome.desktop.interface font-name "Fira Sans Book 10"
+gsettings set org.gnome.desktop.interface font-name "Fira Code weight=450 10"
