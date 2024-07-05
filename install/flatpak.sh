927c83ed2228e8af25baa2f9d9913faa4e98f85d
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-04 17:31:08 -0700

    fix naps2 flatpak

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -13,9 +13,14 @@ flatpak install -y flathub com.github.KRTirtho.Spotube
 flatpak install -y flathub com.github.tchx84.Flatseal
 flatpak install -y flathub com.mattjakeman.ExtensionManager
 flatpak install -y flathub io.missioncenter.MissionCenter
-flatpak install -y flathub com.naps2.Naps2
 flatpak install -y flathub io.github.peazip.PeaZip
 flatpak install -y flathub org.pitivi.Pitivi
 flatpak install -y flathub org.mozilla.Thunderbird
 flatpak install -y flathub org.tigervnc.vncviewer
 flatpak install -y flathub net.werwolv.ImHex
+
+cd /tmp
+wget -O naps2.flatpak "https://github.com/cyanfish/naps2/releases/download/v7.4.2/naps2-7.4.2-linux-x64.flatpak"
+flatpak install -y ./naps2.flatpak
+rm naps2.flatpak
+cd -
