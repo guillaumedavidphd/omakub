7309f10ccdb742da00b3be72b3e35d4e8c733213
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 09:32:36 -0700

    update git installation script to include git-lfs

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ set -e
 
 # Needed for all installers
 sudo apt update -y
-sudo apt install -y curl git unzip
+sudo apt install -y curl git git-lfs unzip
 
 # Ensure computer doesn't go to sleep or lock while installing
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
 # Run installers
-for script in ~/.local/share/omakub/install/*.sh; do source $script; done
+# for script in ~/.local/share/omakub/install/*.sh; do source $script; done
 
 # Upgrade everything that might ask for a reboot last
 sudo apt upgrade -y
