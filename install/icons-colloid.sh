77bb672c96f444f14241bb40a0f6f59f89b596f7
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 09:32:48 -0700

    update Colloid icon theme installation script to use Dracula variant

--- install/icons-colloid.sh
+++ install/icons-colloid.sh
@@ -2,7 +2,7 @@ mkdir -p ~/Documents/Code
 cd ~/Documents/Code
 git clone https://github.com/vinceliuice/Colloid-icon-theme.git
 cd Colloid-icon-theme
-./install.sh -s all -t all
+./install.sh -s dracula -t all
 cd -
 
 # gsettings set org.gnome.desktop.interface icon-theme "Colloid-"
