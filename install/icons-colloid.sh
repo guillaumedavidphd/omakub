5ca8ba80e683a955c7213b2f7bd9568c5bb871c5
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-04 20:45:00 -0700

    add installation script for Colloid icon theme

---
+++ install/icons-colloid.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/Documents/Code
+cd ~/Documents/Code
+git clone https://github.com/vinceliuice/Colloid-icon-theme.git
+cd Colloid-icon-theme
+./install.sh -s all -t all
+cd -
+
+# gsettings set org.gnome.desktop.interface icon-theme "Colloid-"
