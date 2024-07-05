cfc4c23b0f68155497e72a64b4bd3879b8012ece
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-04 20:35:03 -0700

    fix FreeCAD installation script to download correct architecture

--- install/app-freecad.sh
+++ install/app-freecad.sh
@@ -1,4 +1,17 @@
 mkdir -p ~/Applications
 cd ~/Applications
-wget -c $(wget -qO- https://api.github.com/repos/FreeCAD/FreeCAD/releases/latest | grep "browser_download_url.*AppImage" | cut -d \" -f4)
+# Download latest FreeCAD release info
+release_info=$(wget -qO- https://api.github.com/repos/FreeCAD/FreeCAD/releases/latest)
+
+# Extract download URL for x86_64 AppImage
+download_url=$(echo "$release_info" | grep '"browser_download_url.*x86_64.AppImage"' | cut -d \" -f4)
+
+# Check if download URL is found
+if [[ -z "$download_url" ]]; then
+  echo "Error: x86_64 AppImage not found in latest release"
+  exit 1
+fi
+
+# Download the x86_64 AppImage
+wget -c "$download_url"
 cd -
