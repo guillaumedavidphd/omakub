49c0c33c0d62fafac91d96084be5ea3be2720108
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 15:36:50 -0700

    update monospace font to Fira Code 12

--- install/set-fonts.sh
+++ install/set-fonts.sh
@@ -9,6 +9,6 @@ fc-cache -f -v
 cd -
 
 gsettings set org.gnome.desktop.wm.preferences titlebar-font "Fira Sans SemiBold 10"
-gsettings set org.gnome.desktop.interface monospace-font-name "Fira Mono Regular 11"
+gsettings set org.gnome.desktop.interface monospace-font-name "Fira Code 12"
 gsettings set org.gnome.desktop.interface document-font-name "Roboto Slab Regular 11"
 gsettings set org.gnome.desktop.interface font-name "Fira Sans Book 10"
