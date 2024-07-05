d75030730edf24e3c4e5958f5e301a11a3bdf5d7
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 09:32:55 -0700

    add installation script for Jasper GTK theme with Dracula tweaks

---
+++ install/theme-gnome-jasper.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/Documents/Code
+cd ~/Documents/Code
+git clone https://github.com/vinceliuice/Jasper-gtk-theme.git
+cd Jasper-gtk-theme
+./install.sh -l -t blue --tweaks dracula
+cd -
+
+# gsettings set org.gnome.desktop.interface icon-theme "Colloid-"
