f8ebc6dfd333ef9d5a29a53e0def8a2873f4421f
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-04 17:23:00 -0700

    fix install `-y` flag

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -2,20 +2,20 @@ sudo apt install -y flatpak
 sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-flatpak install flathub com.belmoussaoui.Authenticator
-flatpak install flathub com.usebottles.bottles
-flatpak install flathub app.drey.Warp
-flatpak install flathub hu.irl.cameractrls
-flatpak install flathub io.github.flattool.Warehouse
-flatpak install flathub io.github.giantpinkrobots.varia
-flatpak install flathub io.gitlab.adhami3310.Impression
-flatpak install flathub com.github.KRTirtho.Spotube
-flatpak install flathub com.github.tchx84.Flatseal
-flatpak install flathub com.mattjakeman.ExtensionManager
-flatpak install flathub io.missioncenter.MissionCenter
-flatpak install flathub com.naps2.Naps2
-flatpak install flathub io.github.peazip.PeaZip
-flatpak install flathub org.pitivi.Pitivi
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub org.tigervnc.vncviewer
-flatpak install flathub net.werwolv.ImHex
+flatpak install -y flathub com.belmoussaoui.Authenticator
+flatpak install -y flathub com.usebottles.bottles
+flatpak install -y flathub app.drey.Warp
+flatpak install -y flathub hu.irl.cameractrls
+flatpak install -y flathub io.github.flattool.Warehouse
+flatpak install -y flathub io.github.giantpinkrobots.varia
+flatpak install -y flathub io.gitlab.adhami3310.Impression
+flatpak install -y flathub com.github.KRTirtho.Spotube
+flatpak install -y flathub com.github.tchx84.Flatseal
+flatpak install -y flathub com.mattjakeman.ExtensionManager
+flatpak install -y flathub io.missioncenter.MissionCenter
+flatpak install -y flathub com.naps2.Naps2
+flatpak install -y flathub io.github.peazip.PeaZip
+flatpak install -y flathub org.pitivi.Pitivi
+flatpak install -y flathub org.mozilla.Thunderbird
+flatpak install -y flathub org.tigervnc.vncviewer
+flatpak install -y flathub net.werwolv.ImHex
