1b80751a1a2a5c08e8b8d130af02fa3a9db288ca
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-05 15:30:29 -0700

    update font installation script to include Fira Code and Roboto Slab

--- install/set-fonts.sh
+++ install/set-fonts.sh
@@ -1,6 +1,14 @@
 sudo apt install fonts-firacode fonts-roboto
 
-gsettings set org.gnome.desktop.wm.preferences titlebar-font "Fira Code SemiBold 10"
-gsettings set org.gnome.desktop.interface monospace-font-name "Fira Code 11"
+cd /tmp
+git clone https://github.com/pop-os/fonts.git
+cd fonts
+cp fira $HOME/.local/share/fonts -r
+cp roboto-slab $HOME/.local/share/fonts -r
+fc-cache -f -v
+cd -
+
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Fira Sans SemiBold 10"
+gsettings set org.gnome.desktop.interface monospace-font-name "Fira Mono Regular 11"
 gsettings set org.gnome.desktop.interface document-font-name "Roboto Slab Regular 11"
-gsettings set org.gnome.desktop.interface font-name "Fira Code weight=450 10"
+gsettings set org.gnome.desktop.interface font-name "Fira Sans Book 10"
