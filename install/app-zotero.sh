f1db1b015c73a685891c3e29e0208de7d979bd0b
Guillaume David <guillaumedavidphd@gmail.com>
2024-07-04 17:51:48 -0700

    update Zotero installation script and add `sudo`

--- install/app-zotero.sh
+++ install/app-zotero.sh
@@ -1,1 +1,8 @@
-curl -sL https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
+cd /tmp
+wget -O  zotero.sh "https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh"
+sudo bash ./zotero.sh
+rm zotero.sh
+cd -
+
+sudo apt update
+sudo apt install -y zotero
